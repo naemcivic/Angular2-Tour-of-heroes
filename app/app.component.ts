31055a31b5eaef8fe01a90528358810940daabf5
naemcivic <naemcivic@gmail.com>
2016-09-13 16:52:58 -0400

    create a service for data, use promise based calls to fetch data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,6 @@
 import { Hero } from './hero'
-import { Component } from '@angular/core';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -75,15 +63,25 @@ const HEROES: Hero[] = [
             border-radius: 4px 0 0 4px;
         }
     `],
+    providers: [HeroService]
 })
 
-export class AppComponent { 
-    heroes = HEROES;
+export class AppComponent implements OnInit { 
+    ngOnInit (): void {
+        this.getHeroes();
+    }
+    
+    constructor(private heroService: HeroService){}
+    
+    heroes: Hero[];
     title = 'Tour of Heroes';
-    hero = {};
     selectedHero: Hero;
 
     onSelect(hero: Hero): void{
         this.selectedHero = hero
+    }
+    
+    getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
     }
 }
