ca479cd92882c8eb75ca8f45fff0789cb64df1b7
naemcivic <naemcivic@gmail.com>
2016-09-14 17:02:17 -0400

    create router, styles and links

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: "my-app"
-    template: `
-        <h1>{{title}}</h1>
-        <my-heroes></my-heroes>
-    `
+  selector: 'my-app',
+  styleUrls: ['app/app.component.css'],
+  template: `
+     <h1>{{title}}</h1>
+     <nav>
+        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+        <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+     </nav>
+     <router-outlet></router-outlet>
+   `
 })
-
 export class AppComponent {
-    title = 'Tour of Heroes';
-
+  title = 'Tour of Heroes';
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,23 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { routing } from './app.routing';
 
-import { AppComponent }        from './app.component';
+import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }  from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ BrowserModule, 
+             FormsModule,
+             routing 
+           ],
   declarations: [ AppComponent,
                   HeroesComponent, 
-                  HeroDetailComponent
+                  HeroDetailComponent,
+                  DashboardComponent
 
   ],
   providers: [
@@ -19,4 +25,5 @@ import { HeroService } from './hero.service';
   ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
