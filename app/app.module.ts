aee3812667ef90c60e7ef8c249e29f27eaf155b5
naemcivic <naemcivic@gmail.com>
2016-09-14 10:00:36 -0400

    create heroes component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, 
-                  HeroDetailComponent  
+  declarations: [ AppComponent,
+                  HeroesComponent, 
+                  HeroDetailComponent
+
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
