bcc8f9509f87809be2d9adc74eaf087543f6ff59
naemcivic <naemcivic@gmail.com>
2016-09-15 10:18:40 -0400

    import Http module and InMemoryWebApiModule for mock server

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { routing } from './app.routing';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }  from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
+import { routing } from './app.routing';
 
 @NgModule({
   imports: [ BrowserModule, 
              FormsModule,
+             HttpModule, 
+             InMemoryWebApiModule.forRoot(InMemoryDataService),
              routing 
            ],
   declarations: [ AppComponent,
